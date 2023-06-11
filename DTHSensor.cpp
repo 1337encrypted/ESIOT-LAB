c81ed07afc93439c7728bfe023403787a4e5dad1
Yash Herekar <smallaszero@gmail.com>
2023-06-11 14:06:46 +0530

    Create DTHSensor.cpp

---
+++ DTHSensor.cpp
@@ -0,0 +1,19 @@
+DHT11Sensor::DHT11Sensor(int pin)
+{
+ //Write your code here 
+}
+
+float DHT11Sensor::readTemperature()
+{
+ //Write your code here 
+}
+
+float readHumidity()
+{
+  //Write your code here
+}
+
+void display()
+{
+ //Write your code here 
+}
